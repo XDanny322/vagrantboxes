6c24cc031225272db9f6f35c1259cb6b600a1101
Danny Lai <XDanny322@gmail.com>
2018-01-08 05:39:02 +0000

    Using Yum, and no stright RPM for installation for ELK

--- ELK_Stack_Backend/provision.sh
+++ ELK_Stack_Backend/provision.sh
@@ -2,20 +2,29 @@
 #
 # provision.sh for ELK Stack Server
 ########################################################################
-myhostname=elkstackserver
-
 # Install needed tools
 yum -y install wget mlocate vim net-tools telnet
 
-# Make a directory to work in
-mkdir /tmp/installs
-cd /tmp/installs
+###############################################################################
+# Downloading and install java
+yum -y install java
+
+echo '[elasticsearch-6.x]
+name=Elasticsearch repository for 6.x packages
+baseurl=https://artifacts.elastic.co/packages/6.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md' > /etc/yum.repos.d/elasticsearch.repo
+yum clean all
 
 ###############################################################################
 # Downloading Elastic
-#  Go here to find it https://www.elastic.co/downloads/elasticsearch
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.1.1.rpm
-rpm -ivh elasticsearch-6.1.1.rpm
+# Go here to find it https://www.elastic.co/downloads/elasticsearch
+#  wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.1.1.rpm
+#  rpm -ivh elasticsearch-6.1.1.rpm
+yum -y install elasticsearch
 
 # Primary Elastic config file /etc/elasticsearch/elasticsearch.yml
 sed -i 's/#cluster.name: my-application/cluster.name: dlai-monitoring-cluster/g' /etc/elasticsearch/elasticsearch.yml
@@ -25,10 +34,9 @@ sed -i 's/#network.host: 192.168.0.1/network.host: 0.0.0.0/g' /etc/elasticsearch
 # Start the service
 systemctl daemon-reload
 systemctl enable elasticsearch.service
-
-systemctl status elasticsearch
-systemctl start elasticsearch
-systemctl status elasticsearch
+systemctl status elasticsearch.service
+systemctl start elasticsearch.service
+systemctl status elasticsearch.service
 
 # Test the service:
 # sleep 15
@@ -37,9 +45,10 @@ systemctl status elasticsearch
 
 ###############################################################################
 # Downloading LogStash
-#  Go here to find it https://www.elastic.co/downloads/logstash
-wget https://artifacts.elastic.co/downloads/logstash/logstash-6.1.1.rpm
-rpm -ivh logstash-6.1.1.rpm
+# Go here to find it https://www.elastic.co/downloads/logstash
+#  wget https://artifacts.elastic.co/downloads/logstash/logstash-6.1.1.rpm
+#  rpm -ivh logstash-6.1.1.rpm
+yum -y install logstash
 
 # You can test running Logstash manually by:
 #  1) cd /usr/share/logstash/ ; bin/logstash -e 'input { stdin { } } output {  elasticsearch { hosts => ["localhost:9200"] }}'
@@ -49,14 +58,13 @@ rpm -ivh logstash-6.1.1.rpm
 
 # Start the service
 systemctl daemon-reload
-systemctl enable logstash
-
-systemctl status logstash
-systemctl start logstash
-systemctl status logstash
+systemctl enable logstash.service
+systemctl status logstash.service
+systemctl start logstash.service
+systemctl status logstash.service
 
 #Here is the grok setting when we are ready to enable:
-  # input{
+  # echo 'input{
   #   beats{
   #     port => "5043"
   #   }
@@ -77,28 +85,27 @@ systemctl status logstash
   #     index => "%{[@metadata][beat]}-%{+YYYY.MM.dd}"
   #     document_type => "%{[@metadata][type]}"
   #   }
-  # }
+  # }' > /etc/logstash/conf.d/beats.conf
 
 ###############################################################################
 # Downloading kibana
-#  Go here to find it https://www.elastic.co/downloads/kibana
-wget https://artifacts.elastic.co/downloads/kibana/kibana-6.1.1-x86_64.rpm
-rpm -ivh kibana-6.1.1-x86_64.rpm
+# Go here to find it https://www.elastic.co/downloads/kibana
+#  wget https://artifacts.elastic.co/downloads/kibana/kibana-6.1.1-x86_64.rpm
+#  rpm -ivh kibana-6.1.1-x86_64.rpm
+yum -y install kibana
 
 # Change listening interfact to all interfact
 sed -i 's/#server.host: "localhost"/server.host: "0.0.0.0"/g' /etc/kibana/kibana.yml
 # Change server name
-sed -i "s/#server.name: \"your-hostname\"/server.name: \"$myhostname\"/g" /etc/kibana/kibana.yml
+sed -i "s/#server.name: \"your-hostname\"/server.name: \"elkstackserver\"/g" /etc/kibana/kibana.yml
 # Enable default of 'localhost' for elasticsearch url.
 sed -i 's/#elasticsearch.url/elasticsearch.url/g' /etc/kibana/kibana.yml
 # Enable logging
-sed -i "s/#logging.dest: stdout/logging.dest: \/var\/log\/kibana.log/g" /etc/kibana/kibana.yml
-
+sed -i "s/#logging.dest: stdout/logging.dest: \/tmp\/kibana.log/g" /etc/kibana/kibana.yml
 
 # Start the service
 systemctl daemon-reload
 systemctl enable kibana
-
 systemctl status kibana
 systemctl start kibana
 systemctl status kibana
--- ELK_Stack_Client/provision.sh
+++ ELK_Stack_Client/provision.sh
@@ -2,50 +2,55 @@
 #
 # provision.sh for ELK Stack Client
 ########################################################################
-myhostname=elkstackclient
-elkserver=192.168.56.110
-
 # Install needed tools
 yum -y install wget mlocate vim net-tools telnet
 
-# Make a directory to work in
-mkdir /tmp/installs
-cd /tmp/installs
+
+echo '[elasticsearch-6.x]
+name=Elasticsearch repository for 6.x packages
+baseurl=https://artifacts.elastic.co/packages/6.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md' > /etc/yum.repos.d/elasticsearch.repo
+yum clean all
 
 ###############################################################################
 # Downloading FileBeat
-#  Go here to find it https://www.elastic.co/downloads/beats/filebeat
-wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-6.1.1-x86_64.rpm
-rpm -ivh filebeat-6.1.1-x86_64.rpm
+# Go here to find it https://www.elastic.co/downloads/beats/filebeat
+#  wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-6.1.1-x86_64.rpm
+#  rpm -ivh filebeat-6.1.1-x86_64.rpm
+yum -y install filebeat
 
-# Change filebeat from monitoring all /var/log/*.log to only /var/log/message.log for testing
-sed -i "s/*.log/messages\n  document_type: syslog/g" /etc/filebeat/filebeat.yml
+# Make a backup of the config file
+cp -v /etc/filebeat/filebeat.yml /etc/filebeat/filebeat.yml.orginal
 
-# Changing tags. Tags are meta data added per box
-sed -i 's/#tags:/tags:/g' /etc/filebeat/filebeat.yml
-sed -i 's/service-X/dlaitestserver/g' /etc/filebeat/filebeat.yml
-sed -i 's/web-tier/tesbox/g' /etc/filebeat/filebeat.yml
-
-# Adding custom fields
-sed -i 's/#fields:/fields:/g' /etc/filebeat/filebeat.yml
-sed -i 's/#  env: staging/  dlai_environment: production/g' /etc/filebeat/filebeat.yml
-
-# Disable ElasticSearch output, since we will be sending to Logstash
-sed -i 's/output.elasticsearch:/#output.elasticsearch:/g' /etc/filebeat/filebeat.yml
-sed -i "s/hosts: \[\"localhost:9200\"\]/#hosts: \[\"localhost:9200\"\]/g" /etc/filebeat/filebeat.yml
-
-# Enable logstash
-sed -i 's/#output.logstash:/output.logstash:/g' /etc/filebeat/filebeat.yml
-sed -i "s/#hosts: \[\"localhost:5044\"\]/hosts: \[\"$elkserver:5044\"\]/g" /etc/filebeat/filebeat.yml
-
-
-# Do i need to enable some template?
+echo 'filebeat.prospectors:
+- type: log
+  enabled: false
+  paths:
+    - /var/log/messages
+filebeat.config.modules:
+  path: ${path.config}/modules.d/*.yml
+  reload.enabled: false
+setup.template.settings:
+  index.number_of_shards: 3
+tags: ["dlaitestserver", "tesbox"]
+fields:
+  dlai_environment: production
+setup.kibana:
+output.logstash:
+  hosts: ["192.168.56.110:5043"]' > /etc/filebeat/filebeat.yml
 
 # Start the service
 systemctl daemon-reload
 systemctl enable filebeat
-
 systemctl status filebeat
 systemctl start filebeat
 systemctl status filebeat
+
+# Do i need to enable some template?
+
+
 
