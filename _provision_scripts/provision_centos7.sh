417637e22290031846d5e9adb21b3dea16e115db
Danny Lai <dlai@portware.com>
2018-02-22 20:09:19 -0500

    Consolidated Centos7 provisioners

---
+++ _provision_scripts/provision_centos7.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# provision for clean centos7
+########################################################################
+# Install needed tools
+yum -y install epel-release
+yum -y install wget mlocate vim net-tools telnet git
+
