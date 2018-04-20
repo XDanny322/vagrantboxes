9f409d9771307137d89d7cced5643a520716db06
Danny Lai <dlai@portware.com>
2018-04-20 19:42:19 -0400

    Added note for starting gitlab

--- _provision_scripts/provision_gitlab.sh
+++ _provision_scripts/provision_gitlab.sh
@@ -12,3 +12,13 @@ curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.rp
 #  ee = Enterprise Ed
 #  ce = Community Ed
 sudo EXTERNAL_URL="http://192.168.6.133" yum install -y gitlab-ee
+
+
+# # Start all GitLab components
+# sudo gitlab-ctl start
+
+# # Stop all GitLab components
+# sudo gitlab-ctl stop
+
+# # Restart all GitLab components
+# sudo gitlab-ctl restart
