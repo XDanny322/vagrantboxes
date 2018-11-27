8d73ab0d4450358698ae9c6b5e66d155a3852709
Danny Lai <hlai@factset.com>
2018-11-27 00:05:44 -0500

    Added Docker container

---
+++ _provision_scripts/provision_docker_ce.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install docker-ce -y
+
+sudo systemctl start docker
+
+sudo docker version
+
+sudo docker info
